dfc04464dd7386b819e20e0546b88dccc06492a3
Raymond Weng <xiang.weng3@gmail.com>
2023-07-14 22:38:56 +0800

    small fix

--- src/components/Landing/LandingFooter.jsx
+++ src/components/Landing/LandingFooter.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { footerLinks, footerContact } from "../../constants";
 const LandingFooter = ({ variant = false }) => {
   return (
-    <div className={`${variant ? "bg-transparent" : "bg-black"}`}>
+    <div className={`${variant ? "bg-transparent" : "bg-black"} text-white`}>
       <div className='xl:max-w-[1920px] flex justify-center xs:px-16 px-4 sm:flex-row flex-col mx-auto relative z-10'>
         <div className=' w-full sm:h-52 h-44 flex justify-between items-center'>
           <img alt='StyleUp Logo Small' src='/images/logoFooter.svg'></img>
@@ -14,26 +14,26 @@ const LandingFooter = ({ variant = false }) => {
         </div>
         <ul className='list-none sm:justify-end sm:items-center flex sm:flex-row flex-col-reverse items-start justify-start'>
           <li
-            className={`font-inter text-[16px] text-white font-normal mr-12 whitespace-nowrap`}
+            className={`font-inter text-[16px]font-normal mr-12 whitespace-nowrap`}
             key={0}
           >
             {footerContact}
           </li>
           {footerLinks.map((nav, index) => (
             <li
-              className={`font-inter underline cursor-pointer text-[16px] text-white font-normal mr-12 whitespace-nowrap`}
+              className={`font-inter underline cursor-pointer text-[16px]font-normal mr-12 whitespace-nowrap`}
               key={nav.id}
             >
               <a href={`${nav.link}`}>{nav.title}</a>
             </li>
           ))}
         </ul>
-        <span className='sm:hidden text-white text-[13px] font-normal leading-none mt-[30px] mb-[30px]'>
+        <span className='sm:hidden text-[13px] font-normal leading-none mt-[30px] mb-[30px]'>
           © Copyright 2023 StyleUp
         </span>
       </div>
       {variant && (
-        <div className='bg-[url("/images/wave.svg")] absolute -top-24 w-[200%] h-[200%]'></div>
+        <div className='bg-[url("/images/wave.svg")] bg-no-repeat md:bg-auto bg-cover md:bg-left bg-center absolute -top-24 w-[200%] h-[200%]'></div>
       )}
     </div>
   );
